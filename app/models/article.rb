2d446f889f67641dcf572cabfcf015ec72ddd7f6
Jakub Pajak <jakub.l.pajak@gmail.com>
2018-01-31 13:43:13 -0500

    fixed article len

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   
   validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 5000 }
   
   belongs_to :player
   
